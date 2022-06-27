60c0c43d833f057120eb15b0010f1ace0669894f
Rejka Permana <rejkamuhammad@gmail.com>
2022-06-27 19:53:11 +0700

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :role
   before_validation :set_default_role
 
   def set_default_role
